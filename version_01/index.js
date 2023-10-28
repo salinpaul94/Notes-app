cd8a80b72513b120def72543832d3db82b105cb6
salinpaul valooran <salinpv@hotmail.com>
2023-10-28 14:14:30 +0530

    update btowser storage and fetch data from storage when page loaded

--- version_01/index.js
+++ version_01/index.js
@@ -2,6 +2,14 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
@@ -9,10 +17,23 @@ createBtn.addEventListener("click", () => {
   inputBox.setAttribute("contenteditable", "true");
   img.src = "images/delete.png"
   notesContainer.appendChild(inputBox).appendChild(img);
-});
+
+})
 
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach(nt => {
+      nt.onkeyup = function () {
+        updateStorage();
+      }
+    })
   }
 })
+
+
+
+window.addEventListener("DOMContentLoaded", showNotes());
